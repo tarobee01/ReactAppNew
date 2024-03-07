17a1b77de77c7d6326dfb182ce7e90b7306c1bbc
E-moL0587 <kai.pika.025@outlook.com>
2024-03-07 22:01:12 +0900

    update

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -4,7 +4,7 @@ const Title = ({ setActivePage }) => {
   return (
     <div>
       <h1 className='title1'>スポイト<br />カレンダー</h1>
-      <img className='title2' src="./vite.svg" alt="sample" width="100px" />
+      <img className='title2' src="./rogo.png" alt="sample" width="100px" />
       <button className='title3' onClick={() => setActivePage('main')}>スタート</button>
     </div>
   );
