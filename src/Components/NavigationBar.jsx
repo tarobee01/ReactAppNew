5d0553d01d8fd00c3486b878396db9426ea970ec
tarobee01 <monchichi0124@gmail.com>
2024-03-09 04:24:52 +0900

    パレットのUIを改善しました

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -4,12 +4,17 @@ import Palette from './Palette';
 const NavigationBar = ({ setActivePage }) => {
   const [showPalette, setShowPalette] = useState(false);
 
+  const toggleShowPalette = () => {
+    setShowPalette(!showPalette);
+    console.log('showPalette:', !showPalette); // 状態が更新されたことを確認
+  };
+
   return (
     <div>
       <div className='navigationBar'>
         <button onClick={() => setActivePage('set')}>＞</button>
         <button onClick={() => setActivePage('main')}>◎</button>
-        <button onClick={() => setShowPalette(!showPalette)}>
+        <button onClick={toggleShowPalette}>
           {showPalette ? 'Close Palette' : 'Show Palette'}
         </button>
       </div>
--- src/Components/Palette.jsx
+++ src/Components/Palette.jsx
@@ -6,18 +6,41 @@ function Palette({ showPalette }) {
   const [showAddColorForm, setShowAddColorForm] = useState(false);
   const [newColor, setNewColor] = useState('#ffffff');
   const [newColorText, setNewColorText] = useState('');
+  const [selectedText, setSelectedText] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const addColorToPalette = () => {
-    setPalette([...palette, { color: newColor, text: newColorText }]);
-    // setNewColor('#ffffff'); // この行を削除
-    setNewColorText('');
+    // テキストフィールドが空かどうかチェック
+    if (!newColorText.trim()) {
+      setErrorMessage('テキストを入力してください');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 1000);
+      return;
+    }
+    // 同じ色が既にパレットに存在するかチェック
+    const isColorExists = palette.some(item => item.color === newColor);
+    if (!isColorExists) {
+      setPalette([...palette, { color: newColor, text: newColorText }]);
+      setNewColorText('');
+      setErrorMessage(''); // エラーメッセージをクリア
+    } else {
+      // 同じ色が既に存在する場合、エラーメッセージを設定
+      setErrorMessage('同じ色は追加できません');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 1000);
+    }
   };
 
-  const handleSelectColor = (color) => {
+  const handleSelectColor = (color, text) => {
     if (selectedColor === color) {
+      // 同じ色を再度選択した場合、選択を解除
       setSelectedColor('');
+      setSelectedText('');
     } else {
       setSelectedColor(color);
+      setSelectedText(text);
     }
   };
 
@@ -29,71 +52,142 @@ function Palette({ showPalette }) {
   const getColorStyle = (color) => ({
     backgroundColor: color,
     margin: '0 5px',
-    opacity: selectedColor === color ? '0.5' : '1',
+    width: '80px',
+    height: '55px',
+    textAlign: 'center',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.2)', // Added shadow for depth
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
   });
 
+
   return (
-    <div style={{ position: 'fixed', bottom: '10px', right: '10px', display: 'flex' }}>
+    <div style={{ position: 'fixed', bottom: '10px', right: '0px', display: 'flex'}}>
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
       {showPalette && (
+        //パレットのフレーム
         <div style={{
           position: 'fixed',
-          bottom: '50px',
+          bottom: '57px',
           right: '10px',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'flex-end',
           padding: '5px',
-          borderRadius: '5px',
-          backgroundColor: '#f0f0f0',
-          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
+          maxWidth: 'calc(100% - 30px)',
+          borderRadius: '8px',
+          boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.25)',
+          backgroundColor: '#FEFEFA', // Adjusted for a semi-transparent look
+          backdropFilter: 'blur(10px)', // This will give a frosted glass effect
         }}>
-          <button onClick={() => setShowAddColorForm(!showAddColorForm)} style={{ marginBottom: '10px' }}>
-            {showAddColorForm ? '×' : 'ADD Color'}
-          </button>
-          {showAddColorForm && (
-            <div style={{ marginBottom: '10px' }}>
-              <input type="color" value={newColor} onChange={(e) => setNewColor(e.target.value)} />
-              <input type="text" value={newColorText} onChange={(e) => setNewColorText(e.target.value)} placeholder="Color text" />
-              <button onClick={addColorToPalette}>Save</button>
+          {selectedColor && (
+            <div style={{ padding: '5px', backgroundColor: '#f0f0f0', borderRadius: '5px', marginBottom: '10px' }}>
+              <p>Selected Color: <span style={{ color: selectedColor }}>{selectedColor}</span></p>
+              <p>Selected Text: <span style={{ fontWeight: 'bold' }}>{selectedText}</span></p>
             </div>
           )}
-          <div style={{ display: 'flex' }}>
+          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginBottom: '6px'}}>
             {palette.map((item, index) => (
-              <div key={index} style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0 5px' }}>
+              <div key={index} style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '7px', borderRadius: '10px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>
                 <button
                   style={{
                     backgroundColor: item.color,
-                    width: '80px',
-                    height: '55px',
+                    width: '55px',
+                    height: '35px',
                     textAlign: 'center',
+                    border: item.color === selectedColor ? '3px solid black' : 'none', // 選択された要素の枠線を太くする
+                    borderRadius: '10px 10px 0 0',
+                    position: 'relative',
+                    color: '#333333',
+                    fontSize: '14px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    fontWeight: 'bold',
+                    fontFamily: 'Arial, sans-serif',
                   }}
-                  onClick={() => handleSelectColor(item.color)}
+                  onClick={() => handleSelectColor(item.color, item.text)}
                 >
+                  <div
+                    style={{
+                      position: 'absolute',
+                      bottom: '0',
+                      right: '0',
+                      color: '#333333',
+                      fontSize: '10px',
+                      fontWeight: 'bold',
+                      fontFamily: 'Arial, sans-serif',
+                      padding: '2px', // テキストとオブジェクトの境界線との間に余白を設定
+                    }}
+                  >
+                    {item.text}
+                  </div>
                 </button>
                 <button
                   style={{
                     position: 'absolute',
-                    top: '-10px',
-                    right: '73px',
-                    width: '20px',
-                    height: '20px',
-                    lineHeight: '20px',
-                    textAlign: 'center',
-                    backgroundColor: 'transparent',
+                    top: '-6px',
+                    right: '-6px',
+                    width: '15px',
+                    height: '15px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: 'rgba(44, 62, 80, 0.5)',
                     border: 'none',
-                    color: 'red',
+                    color: 'white',
                     cursor: 'pointer',
-                    fontSize: '30px'
+                    fontSize: '10px',
+                    borderRadius: '50%',
                   }}
                   onClick={() => removeColorFromPalette(index)}
                 >
-                  ×
+                  <i className="fas fa-times"></i>
                 </button>
-                <div style={{ color: 'black', textAlign: 'center', marginTop: '5px' }}>
-                  {item.text}
-                </div>
               </div>
             ))}
+          </div>
+          
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <input
+              type="color"
+              value={newColor}
+              onChange={(e) => setNewColor(e.target.value)}
+              style={{
+                width: '30px',
+                height: '30px',
+                cursor: 'pointer',
+                marginRight: '6px'
+              }}
+            />
+            <input
+              type="text"
+              value={newColorText}
+              onChange={(e) => setNewColorText(e.target.value)}
+              placeholder="Color text"
+              style={{
+                flexGrow: 1,
+                border: '1px solid #ddd',
+                borderRadius: '20px',
+                padding: '5px 10px',
+                marginRight: '6px'
+              }}
+            />
+            <button
+              onClick={addColorToPalette}
+              style={{
+                backgroundColor: '#2c3e50',
+                color: 'white',
+                border: 'none',
+                borderRadius: '20px',
+                padding: '5px 15px',
+                cursor: 'pointer',
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)'
+              }}
+            >
+              Save
+            </button>
           </div>
         </div>
       )}
